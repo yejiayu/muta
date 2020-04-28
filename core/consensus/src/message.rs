3421609787946bed95bedb9697c3a7d6cd8c136b
yejiayu <yejiayu.fe@gmail.com>
2020-04-28 17:41:27 +0800

    add log

--- core/consensus/src/message.rs
+++ core/consensus/src/message.rs
@@ -286,12 +286,13 @@ impl<R: Rpc + 'static, S: Storage + 'static> MessageHandler for PullTxsRpcHandle
     async fn process(&self, ctx: Context, msg: PullTxsRequest) {
         let now = std::time::Instant::now();
         let len = msg.inner.len();
-        let futs = msg
-            .inner
-            .into_iter()
-            .map(|tx_hash| self.storage.get_transaction_by_hash(tx_hash))
-            .collect::<Vec<_>>();
-        let ret = try_join_all(futs).await.map(FixedSignedTxs::new);
+        let ret = self.storage.get_transactions(msg.inner).await.map(FixedSignedTxs::new);
+        // let futs = msg
+        //     .inner
+        //     .into_iter()
+        //     .map(|tx_hash| self.storage.get_transaction_by_hash(tx_hash))
+        //     .collect::<Vec<_>>();
+        // let ret = try_join_all(futs).await.map(FixedSignedTxs::new);
         log::error!("[push_sync_txs_from_sync]: size {:?} cost {:?}", len, now.elapsed());
         self.rpc
             .response(ctx, RPC_RESP_SYNC_PULL_TXS, ret, Priority::High)
